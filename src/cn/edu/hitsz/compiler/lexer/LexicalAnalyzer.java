af61066ca1f776b904f32a1f862441835f33cbc4
Sun Duo <1075079562@qq.com>
2022-09-20 13:55:17 +0800

    update

--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -4,6 +4,11 @@ import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.StreamSupport;
 
 /**
@@ -16,9 +21,13 @@ import java.util.stream.StreamSupport;
  */
 public class LexicalAnalyzer {
     private final SymbolTable symbolTable;
+    private final StringBuilder codeWords;
+    private final List<String> keyWords;
 
     public LexicalAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
+        this.codeWords = new StringBuilder();
+        this.keyWords = Arrays.asList("int", "return");
     }
 
 
@@ -31,7 +40,16 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        try (BufferedReader inputCode = new BufferedReader(new FileReader(path))) {
+            String codeLine;
+            while (Objects.nonNull(codeLine = inputCode.readLine())) {
+                for (String tmpWord : codeLine.split(" ")) {
+                    codeWords.append(tmpWord);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -40,8 +58,25 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
+
+        for (Character letter : codeWords.toString().toCharArray()) {
+            System.out.println(letter);
+
+        }
+
+
+        // 在你的实现的某个地方...
+        /*if (!symbolTable.has(identifierText)) {
+            symbolTable.add(identifierText);
+        }*/
+
         throw new NotImplementedException();
     }
+
+    class StateMachine {
+
+    }
+
 
     /**
      * 获得词法分析的结果, 保证在调用了 run 方法之后调用
@@ -53,7 +88,15 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
+
+        // test
+//        List<Token> tokens = new ArrayList<>();
+//        tokens.add(Token.simple("Semicolon"));
+//        tokens.add(Token.simple("+"));
+//        tokens.add(Token.normal("id", "resultaaa"));
         throw new NotImplementedException();
+//        return tokens;
+
     }
 
     public void dumpTokens(String path) {
