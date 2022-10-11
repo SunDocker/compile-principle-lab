c38c27a824e58640621181c5bb1239a5015bf74c
Sun Duo <1075079562@qq.com>
2022-10-11 16:19:12 +0800

    update

--- src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
@@ -2,13 +2,10 @@ package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
-import cn.edu.hitsz.compiler.parser.table.LRTable;
-import cn.edu.hitsz.compiler.parser.table.Production;
-import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.*;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //TODO: 实验二: 实现 LR 语法分析驱动程序
 
@@ -23,6 +20,15 @@ import java.util.List;
 public class SyntaxAnalyzer {
     private final SymbolTable symbolTable;
     private final List<ActionObserver> observers = new ArrayList<>();
+
+    // 输入的token序列
+    private final Deque<Token> tokenDeque = new ArrayDeque<>();
+    // 符号栈
+    private final Stack<Symbol> symbolStack = new Stack<>();
+    // 状态栈
+    private final Stack<Status> statusStack = new Stack<>();
+    // LR分析表
+    private LRTable lrTable;
 
 
     public SyntaxAnalyzer(SymbolTable symbolTable) {
@@ -79,14 +85,16 @@ public class SyntaxAnalyzer {
         // 你可以自行选择要如何存储词法单元, 譬如使用迭代器, 或是栈, 或是干脆使用一个 list 全存起来
         // 需要注意的是, 在实现驱动程序的过程中, 你会需要面对只读取一个 token 而不能消耗它的情况,
         // 在自行设计的时候请加以考虑此种情况
-        throw new NotImplementedException();
+        tokens.forEach(this.tokenDeque::offer);
+        symbolStack.push(new Symbol(Token.eof()));
     }
 
     public void loadLRTable(LRTable table) {
         // TODO: 加载 LR 分析表
         // 你可以自行选择要如何使用该表格:
         // 是直接对 LRTable 调用 getAction/getGoto, 抑或是直接将 initStatus 存起来使用
-        throw new NotImplementedException();
+        this.lrTable = table;
+        statusStack.push(lrTable.getInit());
     }
 
     public void run() {
@@ -94,6 +102,84 @@ public class SyntaxAnalyzer {
         // 你需要根据上面的输入来实现 LR 语法分析的驱动程序
         // 请分别在遇到 Shift, Reduce, Accept 的时候调用上面的 callWhenInShift, callWhenInReduce, callWhenInAccept
         // 否则用于为实验二打分的产生式输出可能不会正常工作
-        throw new NotImplementedException();
+        while (!tokenDeque.isEmpty()) {
+            final var action = lrTable.getAction(statusStack.peek(), tokenDeque.peek());
+            var isAccept = false;
+            var isError = false;
+            switch (action.getKind()) {
+                case Shift -> {
+                    // TODO 是之前还是之后call？
+                    // callWhenInShift(statusStack.peek(), tokenDeque.peek());
+                    statusStack.push(action.getStatus());
+                    symbolStack.push(new Symbol(tokenDeque.poll()));
+                }
+                case Reduce -> {
+                    final var production = action.getProduction();
+                    // TODO 是之前还是之后call？
+                    callWhenInReduce(statusStack.peek(), production);
+                    var len = production.body().size();
+                    while (len-- != 0) {
+                        statusStack.pop();
+                        symbolStack.pop();
+                    }
+                    symbolStack.push(new Symbol(production.head()));
+                    final var gotoStatus = lrTable.getGoto(statusStack.peek(), symbolStack.peek().getNonTerminal());
+                    statusStack.push(gotoStatus);
+                }
+                case Accept -> {
+                    isAccept = true;
+                    callWhenInAccept(statusStack.peek());
+                }
+                case Error -> isError = true;
+            }
+
+            // TODO: 关于Accept和Error的处理
+            if (isAccept) {
+                break;
+            } else if (isError) {
+                throw new RuntimeException("语法分析出错");
+            }
+        }
+
+
+    }
+
+    static class Symbol {
+        Token token;
+        NonTerminal nonTerminal;
+
+        public Symbol(Token token){
+            this.token = token;
+            this.nonTerminal = null;
+        }
+
+        public Symbol(NonTerminal nonTerminal){
+            this.token = null;
+            this.nonTerminal = nonTerminal;
+        }
+
+        public boolean isToken(){
+            return this.token != null;
+        }
+
+        public boolean isNonTerminal(){
+            return this.nonTerminal != null;
+        }
+
+        public Token getToken() {
+            if (isToken()) {
+                return token;
+            }
+            // TODO 异常信息
+            throw new RuntimeException("This symbol is not a token !");
+        }
+
+        public NonTerminal getNonTerminal() {
+            if (isNonTerminal()) {
+                return nonTerminal;
+            }
+            // TODO 异常信息
+            throw new RuntimeException("This symbol is not a non terminal !");
+        }
     }
 }
